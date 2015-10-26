5d2dd179e714011f35db7f127b4d6c272043c570
Ly Nguyen <Ly900@aol.com>
2015-10-26 10:52:56 -0400

    script - able to grab src of image clicked

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ $(document).ready(function() {
 
 $("img").hide();
 
+// var clickCounter = 0;
+
 $("td").click(function () {
-  $(this).find("img").show();
-});
-
-
-
+  var image1 = $(this).find("img").show().attr("src");
+  console.log(image1);
+//   clickCounter = clickCounter + 1;
+//
+//   $("td").click(function () {
+//     $(this).find("img").show();
+//     var image2 = $(this).find("img");
+//     if ()
+//
+//
+// });
+}); // ends 1st td click event
 
 
 
