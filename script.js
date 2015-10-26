75e7632da3418d8087b6ca5d023e3e5bbd76c926
Ly Nguyen <Ly900@aol.com>
2015-10-26 11:10:30 -0400

    able to grab the src of two images and store in variables but cannot access those variables from another function to compare them

--- script.js
+++ script.js
@@ -2,22 +2,31 @@ $(document).ready(function() {
 
 $("img").hide();
 
-// var clickCounter = 0;
+function compareImages() {
+  if (image1 == image2) {
+    console.log("You've got a match!");
+  }
+}
 
+function secondClick() {
+$("td").click(function secondClick() {
+  var image2 = $(this).find("img").show().attr("src");
+  console.log(image2);
+  compareImages()
+}); // ends 2nd click event
+}
+
+function firstClick() {
 $("td").click(function () {
   var image1 = $(this).find("img").show().attr("src");
   console.log(image1);
-//   clickCounter = clickCounter + 1;
-//
-//   $("td").click(function () {
-//     $(this).find("img").show();
-//     var image2 = $(this).find("img");
-//     if ()
-//
-//
-// });
-}); // ends 1st td click event
+  secondClick();
+}); // ends 1st click event
+}
 
 
 
-});
+firstClick();
+
+
+}); //ends ready method
