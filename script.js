dfddc5fcb9cd33e43579429741ef0312bede5695
Ly Nguyen <Ly900@aol.com>
2015-10-26 19:02:29 -0400

    script - matched cards stay put, unmatched cards hide right away; array emptied after

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 $(document).ready(function() {
 
 $("img").hide();
-var clickedCards = [];
+clickedCards = [];
+var clickCounter = 0;
+// at clickCounter = 0 and even numbers, nothing happens
 
-function findMatch () {
-  var image;
-  $("td").click(function () {
-  image = $(this).find("img").show().attr("src");
-  clickedCards.push(image)
-  console.log(clickedCards)
-  $(this).off();
-  });
-}; //ends function findMatch
+$("td").click(function () {
+    var image = $(this).find("img");
+    image.show();
+    clickedCards.push(image);
+    if (clickCounter % 2 !== 0) {
+      if (clickedCards[0][0].getAttribute("src") == clickedCards[1][0].getAttribute("src")) {
+      console.log("match");
+      clickedCards = [];
+      }
+      else {
+        console.log("don't match");
+        ($(clickedCards[0])).hide();
+        ($(clickedCards[1])).hide();
+        clickedCards = [];
+      }
+    } //ends if clickCount is odd
+    clickCounter++;
 
-findMatch();
+  })
+
+
+
+//   compareCards();
+// }; //ends function findMatch
+//
+// function compareCards () {
+//   while (clickedCards === !"") {
+//
+//   } //ends while loop
+// } //ends compareCards function
+
+
 
 }); //ends ready method
