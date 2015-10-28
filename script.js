3db529a0041ee6be0741f17beb7251b92072e710
Ly Nguyen <Ly900@aol.com>
2015-10-28 10:42:24 -0400

    dynamically add images inside divs with indices array

--- script.js
+++ script.js
@@ -1,49 +1,65 @@
 $(document).ready(function() {
 
-$("img").hide();
-clickedCards = [];
-var clickCounter = 0;
-var correctPairs = 0;
+allCards = [
+"images/annoyed-cat.jpg",
+"images/cat-bathing.jpg",
+"images/cat-tongue.jpg",
+"images/cat-wig.jpg",
+"images/cat-crosseyed.jpg",
+"images/cat-green-ears.jpg",
+"images/grumpy-cat.jpg",
+"images/cat-with-glasses.jpg"
+];
 
-function resetGame () {
-  $("img").hide();
-  var clickedCards = [];
-  var clickCounter = 0;
-  $("#clickNumber").html(clickCounter);
-} 
+// $("img").hide();
+// clickedCards = [];
+// var clickCounter = 0;
+// var correctPairs = 0;
 
-$("#resetButton").on("click", resetGame);
+$.each(allCards, function (index, value) {
+  $("<div class='imageDiv'><img></img></div>").find("img").attr("src", value).appendTo("#cardsContainer");
+})
 
-$("td").click(function () {
-    var image = $(this).find("img");
-    image.show();
-    clickedCards.push(image);
-    image.click(false);
 
-    if (clickCounter % 2 !== 0) {
-      var card1 = clickedCards[0][0].getAttribute("src");
-      var card2 = clickedCards[1][0].getAttribute("src");
-      if (card1 == card2 ) {
-      console.log("match");
-      clickedCards = [];
-      correctPairs++;
-        if (correctPairs === 8) {
-          alert("You've won the game!")
-        }
-      }
-      else {
-        console.log("don't match");
-        image1 = ($(clickedCards[0]));
-        image2 = ($(clickedCards[1]));
-        setTimeout("image1.fadeOut()", 1000);
-        setTimeout("image2.fadeOut()", 1000);
-        clickedCards = [];
-      }
-    } //ends if clickCount is odd
-    clickCounter++;
-    $("#clickNumber").html(clickCounter);
-    console.log(clickCounter);
-  })
+// function resetGame () {
+//   $("img").hide();
+//   var clickedCards = [];
+//   var clickCounter = 0;
+//   $("#clickNumber").html(clickCounter);
+// }
+//
+// $("#resetButton").on("click", resetGame);
+//
+// $("td").click(function () {
+//     var image = $(this).find("img");
+//     image.show();
+//     clickedCards.push(image);
+//     image.click(false);
+//
+//     if (clickCounter % 2 !== 0) {
+//       var card1 = clickedCards[0][0].getAttribute("src");
+//       var card2 = clickedCards[1][0].getAttribute("src");
+//       if (card1 == card2 ) {
+//       console.log("match");
+//       clickedCards = [];
+//       correctPairs++;
+//         if (correctPairs === 8) {
+//           alert("You've won the game!")
+//         }
+//       }
+//       else {
+//         console.log("don't match");
+//         image1 = ($(clickedCards[0]));
+//         image2 = ($(clickedCards[1]));
+//         setTimeout("image1.fadeOut()", 1000);
+//         setTimeout("image2.fadeOut()", 1000);
+//         clickedCards = [];
+//       }
+//     } //ends if clickCount is odd
+//     clickCounter++;
+//     $("#clickNumber").html(clickCounter);
+//     console.log(clickCounter);
+//   })
 
 
 
